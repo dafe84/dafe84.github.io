8209db06056feecd0d918958b56daa8774e8488d
Dafe <mail@dafe.ch>
2025-06-03 01:41:01 +0200

    Revert "Generate Pelican site"
    This reverts commit 42e0f6c3f952448dee1426ffa9f6189bcfb89b77.

---
+++ content/steine.md
@@ -0,0 +1,10 @@
+Title: Steine bemalen
+Date: 2025-03-02 16:00
+Category: Basteln
+
+
+## Steine bemalen
+
+INHALT & BILDER hinzufügen
+
+![Steine](#)
---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+AUTHOR = 'Luc&Len'
+SITENAME = 'Kindcraft'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Europe/Zurich'
+
+DEFAULT_LANG = 'de'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+THEME = './temp/pelican-themes/graymill/'                # Nice & Simple
+# THEME = './temp/pelican-themes/waterspill/'              # Nice but images too wide
+# THEME = './temp/pelican-themes/jojo/'                    # NOK
+# THEME = './temp/pelican-themes/fresh'                    # NOK
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("dafe.ch", "https://dafe.ch/"),
+)
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
